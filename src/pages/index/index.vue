b8956b646e33a945e9c447b5db94b22318c2f73f
EmptyG2018 <Empty.G.2018@Gmail.com>
2022-12-03 15:51:16 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="index">
+    {{ msg }}
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const msg = ref('wwwww');
+
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
